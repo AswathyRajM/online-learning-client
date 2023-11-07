7cd95255b1177170e734964e3a5962e7bcea6f81
AswathyRajM <aswathyraj767@gmail.com>
2023-11-08 02:23:45 +0530

    footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter } from 'react-router-dom';
 import Pages from './pages';
 import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <Pages />
+      <Footer />
     </BrowserRouter>
   );
 }
